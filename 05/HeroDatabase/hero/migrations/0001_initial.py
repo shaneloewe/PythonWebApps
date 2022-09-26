d9de2a0bd11a09ed20bfa2e1b23078567dd49c2c
Shane Loewe <shaneloewe@gmail.com>
2022-09-25 19:54:20 -0600

    added stuff?

--- 05/HeroDatabase/hero/migrations/0001_initial.py
+++ 05/HeroDatabase/hero/migrations/0001_initial.py
@@ -14,7 +14,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Hero',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('heroName', models.CharField(max_length=200)),
                 ('author', models.CharField(max_length=200)),
                 ('origin', models.CharField(max_length=200)),
